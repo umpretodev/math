85aae41262af6eea7177eb7a7be5df519bf37709
umpreto <lebackman18@gmail.com>
2025-11-27 15:03:28 -0300

    ✨ feat: add main

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
 #include "src/heads/sum.h"
+#include "src/heads/sub.h"
+#include "src/heads/mult.h"
+#include "src/heads/div.h"
+#include "src/heads/poww.h"
+#include "src/heads/sqrtt.h"
 
 int main(void) {
-    int s;
-    s = sum(4, 6);
-    printf("s = %d\n", s);
+    float a = 2, b = 3;
+    float sumResult, subResult, multResult, divResult, powResult, sqrtResult;
+
+    sum(a, b, &sumResult);
+    sub(a, b, &subResult);
+    mult(a, b, &multResult);
+    div(a, b, &divResult);
+    poww(a, 2, &powResult);
+    sqrtt(a, &sqrtResult);
+
+    printf("sum(%.2f, %.2f) = %.2f\n", a, b, sumResult);
+    printf("sub(%.2f, %.2f) = %.2f\n", a, b, subResult);
+    printf("mult(%.2f, %.2f) = %.2f\n", a, b, multResult);
+    printf("div(%.2f, %.2f) = %.2f\n", a, b, divResult);
+    printf("pow(%.2f, 2) = %.2f\n", a, powResult);
+    printf("sqrt(%.2f) = %.2f\n", a, sqrtResult);
 
     return 0;
 }
